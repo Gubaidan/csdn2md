c7cbee449814fa20c5c77b48b892b2d6222446bb
gubaidan <492407268@qq.com>
2017-11-21 12:00:00 +0800

    修改 Main

--- src/main/java/com/github/csccoder/csdn2md/Main.java
+++ src/main/java/com/github/csccoder/csdn2md/Main.java
@@ -9,7 +9,12 @@ public class Main {
 
     private static String host = "http://blog.csdn.net";
 
+
     public static void main(String args[]) throws IOException {
+
+
+
+
         //input your blog username
         String author = "xxxxx";
         //绝对路径，确保文件夹下有 md 和 html文件夹，需要自己新建
